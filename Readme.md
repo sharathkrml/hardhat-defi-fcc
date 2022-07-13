97aea95e77254d3bb24999f31790b9da6acbb95e
Sharath <sharathkrml@gmail.com>
2022-07-13 16:25:25 +0530

    deposited collateral

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
-1. Programmatically deposit collateral(Eth/Weth) - we earn yield(% return on deplosited collateral)
-2. Programmatically Borrow another asset : DAI (Stable coin: USD)
-3. Repay the DAI
+1. Programmatically deposit collateral(Eth/Weth) - we earn yield(% return on deplosited collateral) [X]
+2. Programmatically Borrow another asset : DAI (Stable coin: USD)[ ]
+3. Repay the DAI [ ]
--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,12 +1,16 @@
 const { getNamedAccounts, ethers } = require("hardhat")
-const { getWeth } = require("./getWeth")
+const { getWeth, AMOUNT, WETHADDRESS } = require("./getWeth")
 const main = async () => {
     await getWeth()
     // protocol treats everything as ERC20 token
     const { deployer } = await getNamedAccounts()
-    // lending pool address provider :0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
     const lendingPool = await getLendingPool(deployer)
     console.log(`lendingPool address = ${lendingPool.address}`)
+    // approve
+    await approveERC20(WETHADDRESS, lendingPool.address, AMOUNT, deployer)
+    console.log("depositing......")
+    await lendingPool.deposit(WETHADDRESS, AMOUNT, deployer, 0)
+    console.log("Deposited")
 }
 const getLendingPool = async (account) => {
     const lendingPoolAddressProvider = await ethers.getContractAt(
@@ -14,9 +18,16 @@ const getLendingPool = async (account) => {
         "0xb53c1a33016b2dc2ff3653530bff1848a515c8c5",
         account
     )
+    // lendingPoolAddressProvider gives out the address
     const lendingPoolAddress = await lendingPoolAddressProvider.getLendingPool()
     const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
     return lendingPool
+}
+const approveERC20 = async (erc20address, spenderAddress, amountToSpend, account) => {
+    const erc20Token = await ethers.getContractAt("IERC20", erc20address, account)
+    const tx = await erc20Token.approve(spenderAddress, amountToSpend)
+    await tx.wait(1)
+    console.log("approved!!")
 }
 main()
     .then(() => {
